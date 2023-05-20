463a1d499d9ecda23428534e905640ab59b26b17
Heber Pereira <isaiahpereira88@gmail.com>
2023-05-20 11:25:28 -0700

    complete dark mode for projects page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -13,8 +13,11 @@ const FramerImage = motion(Image);
 
 const FeaturedProject = ({ type, title, summary, image, link, githubLink }) => {
   return (
-    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[102%] rounded-[2.5rem] bg-dark rounded-br-3xl" />
+    <article
+      className="w-full flex items-center justify-between rounded-3xl 
+        border border-solid border-dark bg-light shadow-2xl p-12 relative rounded-br-2xl dark:bg-dark dark:border-light"
+    >
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[102%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light" />
       <Link
         href={link}
         target="_blank"
@@ -24,17 +27,21 @@ const FeaturedProject = ({ type, title, summary, image, link, githubLink }) => {
           src={image}
           alt={title}
           placeholder="blur"
-          className="object-cover"
+          className="object-cover dark"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
         />
       </Link>
       <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-3xl mb-2">{type}</span>
+        <span className="text-primary font-medium text-3xl mb-2 dark:text-primaryDark">
+          {type}
+        </span>
         <Link href={link} className="hover:underline underline-offset-2">
-          <h2 className="my-4 w-full text-left text-4xl font-bold">{title}</h2>
+          <h2 className="my-4 w-full text-left text-4xl font-bold dark:text-light">
+            {title}
+          </h2>
         </Link>
-        <p className="my-4 font-medium text-dark ">{summary}</p>
+        <p className="my-4 font-medium text-dark dark:text-light">{summary}</p>
         <div className="my-z flex items-center">
           <Link href={githubLink} target="_blank" className="w-10">
             <GithubIcon />
@@ -42,7 +49,10 @@ const FeaturedProject = ({ type, title, summary, image, link, githubLink }) => {
           <Link
             href={link}
             target="_blank"
-            className="ml-4 rounded-lg bg-dark border border-dark text-light p-2 px-6 text-lg font-semibold hover:bg-light hover:text-dark"
+            className="
+              ml-4 rounded-lg bg-dark border border-dark text-light p-2 px-6 text-lg font-semibold 
+              hover:bg-light hover:text-dark 
+              dark:bg-light dark:text-dark dark:hover:bg-dark dark:hover:text-light dark:hover:border-light"
           >
             Visit Project
           </Link>
@@ -54,8 +64,8 @@ const FeaturedProject = ({ type, title, summary, image, link, githubLink }) => {
 
 const Project = ({ title, type, image, link, githubLink }) => {
   return (
-    <article className="w-full flex flex-col item-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative ">
-      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
+    <article className="w-full flex flex-col item-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl dark:bg-light" />
       <Link
         href={link}
         target="_blank"
@@ -71,7 +81,7 @@ const Project = ({ title, type, image, link, githubLink }) => {
         />
       </Link>
       <div className="w-full flex flex-col items-start justify-between mt-4">
-        <span className="text-primary  mb-2">{type}</span>
+        <span className="text-primary  mb-2 dark:text-primaryDark">{type}</span>
         <Link href={link} className="hover:underline underline-offset-2">
           <h2 className="my-2 w-full text-left text-2xl font-bold">{title}</h2>
         </Link>
@@ -99,7 +109,7 @@ const Projects = () => {
         <title>Isaiah | Projects</title>
         <meta name="description" content="list of completed web projects" />
       </Head>
-      <main className="w-full mb-16 flex flex-col items-center justify-center">
+      <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
             text="Imagination Trumps Knowledge!"
